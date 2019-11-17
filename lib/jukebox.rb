d24edafc24024ed528f6b918be6bd091d1a9d395
Sean Gallivan <seanpgallivan@hotmail.com>
2019-11-17 03:47:37 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
+=======
+
+>>>>>>> 82c5f0ae9e68d2e35614ceca31624d56e8e8c8fa
 def help
   puts "I accept the following commands:"
   puts "- help : displays this help message"
@@ -6,28 +10,46 @@ def help
   puts "- exit : exits this program"
 end
 
+<<<<<<< HEAD
 def list(songs)
   i = 0
   while i < songs.length
     puts "#{i + 1}. #{songs[i]}"
     i += 1
   end
+=======
+def list
+  i = 0
+  puts "#{i + 1}. #{songlist[i]}" while i < songlist.length
+>>>>>>> 82c5f0ae9e68d2e35614ceca31624d56e8e8c8fa
 end
 
 def play(songs)
   puts "Please enter a song name or number:"
   choice = gets.strip
   if (1..songs.length).include?(choice.to_i)
+<<<<<<< HEAD
     puts "Playing " + songs[choice.to_i - 1]
   elsif songs.include?(choice)
     puts "Playing " + songs[songs.index(choice)]
   else
     puts "Invalid input, please try again."
+=======
+    puts songs[choice.to_i - 1]
+  elsif songs.include?(choice)
+    puts songs[songs.index(choice)]
+  else
+    puts "Invalid selection."
+>>>>>>> 82c5f0ae9e68d2e35614ceca31624d56e8e8c8fa
   end
 end
 
 def exit_jukebox
+<<<<<<< HEAD
   puts "Goodbye"
+=======
+  "Goodbye!"
+>>>>>>> 82c5f0ae9e68d2e35614ceca31624d56e8e8c8fa
 end
 
 def run(songs)
@@ -37,8 +59,17 @@ def run(songs)
     puts "Please enter a command:"
     input = gets.strip
     help if input == "help"
+<<<<<<< HEAD
     list(songs) if input == "list"
     play(songs) if input == "play"
   end
   exit_jukebox
 end
+=======
+    list if input == "list"
+    play(songs) if input == "play"
+  end
+  exit_jukebox
+end
+
+>>>>>>> 82c5f0ae9e68d2e35614ceca31624d56e8e8c8fa
